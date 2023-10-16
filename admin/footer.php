f21bb06dbe08800d3b3e1ad0aafaa6311f2f1c7a
vipul arya <113001286+iamvipul1@users.noreply.github.com>
2023-10-16 18:08:52 +0530

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,9 @@
+    <script
+      src="#"
+      integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+      crossorigin="anonymous"></script>   
+    <script src="#" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="#" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
+
